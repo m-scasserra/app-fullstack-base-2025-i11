0183e2a8670ba7b24bdaf7e17f7f319557bf8c2b
Marco Scasserra <marco.scasserra@gmail.com>
2025-06-11 00:52:36 -0300

    Cambiado repo del ts-compiler del Docker Compose. Manejo del evento click y load

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,6 +1,12 @@
+window.addEventListener("load", () => {
 
-function SayHello(){
     let current_value = document.getElementById("textarea_1") as HTMLInputElement;
-    let new_value = "Hello world!!!" + "\n" + current_value.value;
+    let new_value = "Hola mundo!!!" + "\n" + current_value.value;
     document.getElementById("textarea_1").innerHTML = new_value;
-}
+
+    let btn = document.getElementById("btn_1");
+
+    btn.addEventListener("click", () => {
+        alert("Se hizo click");
+    });
+});
